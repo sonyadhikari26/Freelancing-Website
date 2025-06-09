ee598b237b7b6b6e18423e53c1c6a2e7d43abe07
sonyadhikari26 <166592913+sonyadhikari26@users.noreply.github.com>
2025-06-09 15:49:28 +0545

    File initialisation.

---
+++ helper/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: 'deqcggsvp', 
+  api_key: '577398949774843', 
+  api_secret:'v6fcbemSvt_2WWrj7O9Agv_6WqQ',
+});
+
+module.exports = cloudinary;
